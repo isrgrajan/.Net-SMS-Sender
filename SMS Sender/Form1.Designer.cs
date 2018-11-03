0652f359ec299131b9f2ec8c0843cd9457f83479
Isrg Rajan <isrgrajan@outlook.com>
2018-11-03 10:54:12 +0530

    Commit Staged

---
+++ SMS Sender/Form1.Designer.cs
@@ -0,0 +1,336 @@
+﻿namespace SMS_Sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.btn_reset = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.sms_Counter = new System.Windows.Forms.Label();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RbSMS = new System.Windows.Forms.RichTextBox();
+            this.tb_phone = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.rb_diag = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.rb_log = new System.Windows.Forms.RichTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tb_key = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "SMS Sender";
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.btn_reset);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.sms_Counter);
+            this.tabPage1.Controls.Add(this.btn_send);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.RbSMS);
+            this.tabPage1.Controls.Add(this.tb_phone);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(862, 351);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Send SMS";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // btn_reset
+            // 
+            this.btn_reset.Location = new System.Drawing.Point(217, 162);
+            this.btn_reset.Name = "btn_reset";
+            this.btn_reset.Size = new System.Drawing.Size(75, 23);
+            this.btn_reset.TabIndex = 7;
+            this.btn_reset.Text = "Reset";
+            this.btn_reset.UseVisualStyleBackColor = true;
+            this.btn_reset.Click += new System.EventHandler(this.btn_reset_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(381, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(467, 315);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // sms_Counter
+            // 
+            this.sms_Counter.AutoSize = true;
+            this.sms_Counter.Location = new System.Drawing.Point(9, 162);
+            this.sms_Counter.Name = "sms_Counter";
+            this.sms_Counter.Size = new System.Drawing.Size(65, 13);
+            this.sms_Counter.TabIndex = 5;
+            this.sms_Counter.Text = "SMS Size: 0";
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(298, 162);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 4;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "SMS Content:";
+            // 
+            // RbSMS
+            // 
+            this.RbSMS.DetectUrls = false;
+            this.RbSMS.Location = new System.Drawing.Point(9, 59);
+            this.RbSMS.MaxLength = 160;
+            this.RbSMS.Multiline = false;
+            this.RbSMS.Name = "RbSMS";
+            this.RbSMS.Size = new System.Drawing.Size(365, 96);
+            this.RbSMS.TabIndex = 2;
+            this.RbSMS.Text = "";
+            this.RbSMS.WordWrap = false;
+            this.RbSMS.TextChanged += new System.EventHandler(this.RbSMS_TextChanged);
+            // 
+            // tb_phone
+            // 
+            this.tb_phone.Location = new System.Drawing.Point(9, 20);
+            this.tb_phone.Name = "tb_phone";
+            this.tb_phone.Size = new System.Drawing.Size(365, 20);
+            this.tb_phone.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Mobile Number:";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(770, 309);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // rb_diag
+            // 
+            this.rb_diag.Location = new System.Drawing.Point(10, 81);
+            this.rb_diag.Name = "rb_diag";
+            this.rb_diag.ReadOnly = true;
+            this.rb_diag.Size = new System.Drawing.Size(835, 222);
+            this.rb_diag.TabIndex = 5;
+            this.rb_diag.Text = "";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(364, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Ping API Gateway";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(187, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Diagnose network issues";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Clear System Event Logs";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // rb_log
+            // 
+            this.rb_log.Location = new System.Drawing.Point(6, 6);
+            this.rb_log.Name = "rb_log";
+            this.rb_log.ReadOnly = true;
+            this.rb_log.Size = new System.Drawing.Size(850, 339);
+            this.rb_log.TabIndex = 0;
+            this.rb_log.Text = "";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 7);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Isrg SMS API Key";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.rb_log);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(862, 351);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "System Event log";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button5);
+            this.tabPage4.Controls.Add(this.tb_key);
+            this.tabPage4.Controls.Add(this.button4);
+            this.tabPage4.Controls.Add(this.rb_diag);
+            this.tabPage4.Controls.Add(this.button3);
+            this.tabPage4.Controls.Add(this.button2);
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(862, 351);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Settings";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 50);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(870, 377);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tb_key
+            // 
+            this.tb_key.Location = new System.Drawing.Point(10, 24);
+            this.tb_key.Name = "tb_key";
+            this.tb_key.Size = new System.Drawing.Size(757, 20);
+            this.tb_key.TabIndex = 7;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(770, 23);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Save";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 447);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "SMS Sender";
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button btn_reset;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label sms_Counter;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox RbSMS;
+        private System.Windows.Forms.TextBox tb_phone;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox rb_diag;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox rb_log;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox tb_key;
+    }
+}
+
---
+++ SMS Sender/Form1.cs
@@ -0,0 +1,252 @@
+﻿//Get API Key from https://www.isrgrajan.com/sms-api/
+
+using System;using System.Text.RegularExpressions;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace SMS_Sender
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            refreshLog();
+            apimanager();
+        }
+            private void btn_send_Click(object sender, EventArgs e)
+        {
+            string mobile = tb_phone.Text;
+            string sID = tb_key.Text.ToString();
+            if(sID.Equals(""))
+            {
+                MessageBox.Show("SMS API Key is missing");
+            } else { 
+
+            string sms = RbSMS.Text;
+                if (mobile.Equals(""))
+                {
+                    MessageBox.Show("Mobile number should not be empty");
+                }
+                else if (sms.Equals(""))
+                {
+                    MessageBox.Show("SMS Content should not be empty");
+                }
+                else
+                {
+
+                    Match m = Regex.Match(mobile, @"^[789]\d{9}$");
+                    if (!m.Success)
+                    {
+                        MessageBox.Show(mobile + " is not a valid number");
+                    }
+                    else
+                    {
+
+                        try
+                        {
+                            btn_send.Enabled = false;
+
+                            string strUrl = "https://www.isrgrajan.com//api/sms/project/handler.php?phone=" + mobile + "&sms=" + sms + "&sid="+sID;
+                            WebRequest request = HttpWebRequest.Create(strUrl);
+                            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                            Stream s = (Stream)response.GetResponseStream();
+                            StreamReader readStream = new StreamReader(s);
+                            string dataString = readStream.ReadToEnd();
+                            response.Close();
+                            s.Close();
+                            readStream.Close();
+                            string log = dataString.ToString();
+                            log = log.Replace(System.Environment.NewLine, log);
+                            MessageBox.Show("Message has been sent");
+                            writeLog("Content:{Phone: " + mobile + "; SMS: " + sms + "} " + log);
+
+                        }
+                        catch (Exception MSG)
+                        {
+                            MessageBox.Show("Error: Failed to send the SMS. Check logs for detailed information");
+                            var time = DateTime.Now;
+                            string date = time.ToString("dd/MM/yyyy, HH:mm:ss");
+                            writeLog(MSG.ToString());
+                        }
+
+
+                    }
+
+                }
+            }
+        }
+
+        private void btn_reset_Click(object sender, EventArgs e)
+        {
+            tb_phone.Text = "";
+            RbSMS.Text = "";
+            btn_send.Enabled = true;
+        }
+
+        private void refreshLog()
+        {
+            if (File.Exists("sms_log.txt"))
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader("sms_log.txt");
+                rb_log.Text = sr.ReadToEnd();
+                sr.Close();
+
+            }
+            else
+            {
+                rb_log.Text = "Hooray! Enjoy your day. There are no logs";
+            }
+        }
+
+        private void writeLog(string logContents)
+        {
+            var time = DateTime.Now;
+            string date = time.ToString("dd/MM/yyyy, HH:mm:ss");
+
+            if (File.Exists("sms_log.txt"))
+            {
+                using (var tw = new StreamWriter("sms_log.txt", true))
+                {
+                    tw.WriteLine(date + " " + logContents);
+                }
+                refreshLog();
+            }
+            else
+            {
+                File.Create("sms_log.txt").Dispose();
+                using (TextWriter tw = new StreamWriter("sms_log.txt"))
+                {
+
+                    tw.WriteLine(date + " " + logContents);
+                }
+                refreshLog();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            apiWrite(tb_key.Text.ToString());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var ping = new System.Net.NetworkInformation.Ping();
+                var result = ping.Send("www.isrgrajan.com");
+
+                if (result.Status != System.Net.NetworkInformation.IPStatus.Success)
+                    return;
+                for (int i = 0; i <= 5; i++)
+                {
+                    rb_diag.AppendText(result.ToString() + "\n");
+                }
+            } catch(Exception ex)
+            {
+                string abc = ex.ToString();
+                MessageBox.Show("No internet connected");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.Process.Start(
+                "Rundll32.exe", "ndfapi,NdfRunDllDiagnoseIncident");
+            }
+            catch (Exception network)
+            {
+                MessageBox.Show("Error: Failed to start network diagnose");
+                writeLog(network.ToString());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Are you sure you want to clear system logs?", "Clear System logs", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                if (File.Exists("sms_log.txt"))
+                {
+                    File.Create("sms_log.txt").Close();
+                }
+                refreshLog();
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            rb_diag.Text = "";
+        }
+
+        private void RbSMS_TextChanged(object sender, EventArgs e)
+        {
+            Int32 smsSize = Convert.ToInt32(RbSMS.TextLength);
+            sms_Counter.Text = "SMS Size: " + smsSize;
+            if (smsSize > 150)
+            {
+                sms_Counter.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                sms_Counter.ForeColor = System.Drawing.Color.Black;
+            }
+
+            if (smsSize > 159)
+            {
+                btn_send.Enabled = false;
+                sms_Counter.Text = "Word limit reached";
+                sms_Counter.ForeColor = System.Drawing.Color.Red;
+            }
+            else
+            {
+                btn_send.Enabled = true;
+            }
+        }
+
+        private void RbSMS_KeyDown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void apimanager()
+        {
+            if (File.Exists("API.txt"))
+            {
+                    System.IO.StreamReader sr = new System.IO.StreamReader("API.txt");
+                    tb_key.Text = sr.ReadToEnd();
+                    sr.Close();
+            }
+            else
+            {
+                //do nothing
+            }
+        }
+
+        private void apiWrite(string key)
+        {
+            if (File.Exists("API.txt"))
+            {
+                File.Create("API.txt").Close();
+                using (var tw = new StreamWriter("API.txt", true))
+                {
+                    tw.WriteLine(key);
+                }
+            }
+            else
+            {
+                File.Create("API.txt").Dispose();
+                using (TextWriter tw = new StreamWriter("API.txt"))
+                {
+
+                    tw.WriteLine(key);
+                }
+            }
+        }
+    }
+
+
+}
